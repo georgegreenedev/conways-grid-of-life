637cc7f1c5cf007dd1eb5b8a2a79085b5d9550e6
George Greene <georgegreene@outlook.com>
2023-06-09 14:16:58 -0400

    conways-grid-of-life-0.8.0: include rules and grid representation as comments; seed grid with nodes using pseudo-random state;

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+
+    Conway's game of life
+
+    Any live cell with two or three live neighbours survives.
+    Any dead cell with three live neighbours becomes a live cell.
+    All other live cells die in the next generation.
+    Similarly, all other dead cells stay dead.
+    
+    [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]
+
+*/
+
+
+const grid = document.getElementById('game');
+
+seedGrid()
+
+function seedGrid() {
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        cell.style.border = '1px solid black';
+        if (Math.random() < 0.25) {
+            cell.style.backgroundColor = 'black'
+        }
+        grid.appendChild(cell);
+    }
+}
