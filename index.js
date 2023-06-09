e38c308fd7c5feb66d38ecf20b82fb849c9415f6
George Greene <georgegreene@outlook.com>
2023-06-09 14:26:03 -0400

    conways-grid-of-life-0.12.0: function getAliveNeighborCount;

--- index.js
+++ index.js
@@ -50,3 +50,92 @@ function getNextGridState() {
 function isCellAlive(index) {
     return grid.children[index].style.backgroundColor === 'black';
 }
+
+function getAliveNeighborCount(index, state) {
+    let counter = 0;
+
+    // top left corner
+    if (index === 0) {
+        isCellAlive(index + 1) ? ++counter : null;
+        isCellAlive(index + 10) ? ++counter : null;
+        isCellAlive(index + 11) ? ++counter : null;
+    }
+
+    // top right corner
+    if (index === 9) {
+        isCellAlive(index - 1) ? ++counter : null;
+        isCellAlive(index + 10) ? ++counter : null;
+        isCellAlive(index + 9) ? ++counter : null;
+    }
+
+    // bottom left corner
+    if (index === 90) {
+        isCellAlive(index + 1) ? ++counter : null;
+        isCellAlive(index - 10) ? ++counter : null;
+        isCellAlive(index - 9) ? ++counter : null;
+    }
+
+    // bottom right corner
+    if (index === 99) {
+        isCellAlive(index - 1) ? ++counter : null;
+        isCellAlive(index - 10) ? ++counter : null;
+        isCellAlive(index - 11) ? ++counter : null;
+    }
+
+    // top row 
+    if (index > 0 && index < 9) {
+        isCellAlive(index - 1) ? ++counter : null;
+        isCellAlive(index + 1) ? ++counter : null;
+        isCellAlive(index + 9) ? ++counter : null;
+        isCellAlive(index + 10) ? ++counter : null;
+        isCellAlive(index + 11) ? ++counter : null;
+    }
+
+    // bottom row 
+    if (index > 90 && index < 99) {
+        isCellAlive(index - 1) ? ++counter : null;
+        isCellAlive(index + 1) ? ++counter : null;
+        isCellAlive(index - 9) ? ++counter : null;
+        isCellAlive(index - 10) ? ++counter : null;
+        isCellAlive(index - 11) ? ++counter : null;
+    }
+
+    // left side
+    if (index % 10 === 0 && index !== 0 && index !== 90) {
+        isCellAlive(index - 10) ? ++counter : null;
+        isCellAlive(index - 9) ? ++counter : null;
+        isCellAlive(index + 1) ? ++counter : null;
+        isCellAlive(index + 10) ? ++counter : null;
+        isCellAlive(index + 11) ? ++counter : null;
+    }
+
+    // right side
+    if (index % 10 === 9 && index !== 9 && index !== 99) {
+        isCellAlive(index - 10) ? ++counter : null;
+        isCellAlive(index - 11) ? ++counter : null;
+        isCellAlive(index - 1) ? ++counter : null;
+        isCellAlive(index + 10) ? ++counter : null;
+        isCellAlive(index + 9) ? ++counter : null;
+    }
+
+
+    // all other nodes
+    if (index % 10 !== 0 && index % 10 !== 9 && index > 9 && index < 89) {
+        isCellAlive(index - 11) ? ++counter : null;
+        isCellAlive(index - 10) ? ++counter : null;
+        isCellAlive(index - 9) ? ++counter : null;
+        isCellAlive(index - 1) ? ++counter : null;
+        isCellAlive(index + 1) ? ++counter : null;
+        isCellAlive(index + 9) ? ++counter : null;
+        isCellAlive(index + 10) ? ++counter : null;
+        isCellAlive(index + 11) ? ++counter : null;
+    }
+
+    if (state && (counter === 2 || counter === 3)) {
+        nextState.push(1);
+    } else if (!state && counter === 3) {
+        nextState.push(1);
+    } else {
+        nextState.push(0)
+    }
+}
